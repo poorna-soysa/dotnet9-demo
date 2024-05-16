a0f4949a2864cf85755a859caa64040730245104
Poorna Soysa <poornapiumal.soysa@gmail.com>
2024-05-16 05:32:58 +0530

    Added LINQ CountBy and AggregateBy methods

---
+++ DotNet9Demo/LINQAggregateByMethodDemo.cs
@@ -0,0 +1,32 @@
+﻿namespace DotNet9Demo;
+
+/// <summary>
+/// This code is taken from Microsoft GitHub
+/// </summary>
+internal class LINQAggregateByMethodDemo
+{
+    public static void AggregateByDotNet9()
+    {
+        (string id, int score)[] data =
+    [
+        ("0", 42),
+        ("1", 5),
+        ("2", 4),
+        ("1", 10),
+        ("0", 25),
+    ];
+
+        var aggregatedData =
+            data.AggregateBy(
+                keySelector: entry => entry.id,
+                seed: 0,
+                (totalScore, curr) => totalScore + curr.score
+                );
+
+        foreach (var item in aggregatedData)
+        {
+            Console.WriteLine(item);
+        }
+        
+    }
+}
---
+++ DotNet9Demo/LINQCountByMethodDemo.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNet9Demo;
+
+/// <summary>
+/// This code is taken from Microsoft GitHub
+/// </summary>
+internal static class LINQCountByMethodDemo
+{
+    public static void CountByDotNet9()
+    {
+        string sourceText = """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Sed non risus. Suspendisse lectus tortor, dignissim sit amet, 
+    adipiscing nec, ultricies sed, dolor. Cras elementum ultrices amet diam.
+""";
+
+        // Find the most frequent word in the text.
+        KeyValuePair<string, int> mostFrequentWord = sourceText
+            .Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(word => word.ToLowerInvariant())
+            .CountBy(word => word)
+            .MaxBy(pair => pair.Value);
+
+        Console.WriteLine(mostFrequentWord.Key); // amet
+    }
+}
